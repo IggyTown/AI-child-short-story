8cf8889ba496fca7ce4b927fa27cb73c4b198b72
IggyTown <isabeltownsend001@gmail.com>
2025-01-28 21:20:42 +0000

    Improved the UX experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayStory(response) {
-  new Typewriter("#child-story", {
+  new Typewriter("#story", {
     strings: response.data.answer,
     autoStart: true,
     delay: 25,
@@ -13,9 +13,13 @@ function generateStory(event) {
   let userInput = document.querySelector("#user-input");
   let apiKey = "3cc25b050436a2462365bo90t67a8f38";
   let context =
-    "You are a short-story telling AI assistant. Provide interesting, polite, short and fun stories for children that are no more than fifteen lines. Make sure to follow the user instructions. Include the title for the story inside a <strong> element at the start of the story and seperate it from the story with a <br />";
-  let prompt = `User instructions: Generate a short story for a child ${userInput.value}`;
+    "You are a short-story telling AI assistant. Provide interesting, polite, short and fun stories for children that are no more than fifteen lines. Make sure to follow the user instructions. Include the title for the story inside a <strong> element at the start of the story and seperate it from the story with a <br />.";
+  let prompt = `User instructions: Generate a short story for a child about ${userInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let storyElement = document.querySelector("#story");
+  storyElement.classList.remove("hidden");
+  storyElement.innerHTML = `<span class="generating">⏳ </span> Generating a short story for you about ${userInput.value}`;
 
   axios.get(apiUrl).then(displayStory);
 }
